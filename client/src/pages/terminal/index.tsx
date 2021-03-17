d853cab09e8bbc43de774e28587de5e190366fe2
Rasukarusan <gta_youtube@yahoo.co.jp>
2021-03-17 11:22:08 +0900

    nestjsでWebSocket実装。clientと疎通確認済み。

--- client/src/pages/terminal/index.tsx
+++ client/src/pages/terminal/index.tsx
@@ -1,7 +1,8 @@
 import dynamic from 'next/dynamic'
 import { Layout } from '@/components/layout'
 import { makeStyles } from '@material-ui/core/styles'
-import { Grid } from '@material-ui/core'
+import { Grid, Button } from '@material-ui/core'
+import io from 'socket.io-client'
 
 const Terminal = dynamic(
   () => import('@/components/terminal').then((mod) => mod.TerminalComponent),
@@ -18,10 +19,35 @@ const useStyles = makeStyles({
 })
 const Index: React.FC = () => {
   const classes = useStyles()
+  const handleClick = () => {
+    console.log('click')
+    const socket = io('http://localhost:3001')
+
+    socket.on('connect', function () {
+      console.log('Connected')
+
+      socket.emit('events', { test: 'test' })
+      socket.emit('identity', 0, (response) =>
+        console.log('Identity:', response)
+      )
+    })
+    socket.on('events', function (data) {
+      console.log('event', data)
+    })
+    socket.on('exception', function (data) {
+      console.log('event', data)
+    })
+    socket.on('disconnect', function () {
+      console.log('Disconnected')
+    })
+  }
   return (
     <Layout>
       <div className={classes.container}>
         <Terminal id="terminal" />
+        <Button onClick={handleClick} color="primary" variant="contained">
+          Click
+        </Button>
       </div>
     </Layout>
   )
