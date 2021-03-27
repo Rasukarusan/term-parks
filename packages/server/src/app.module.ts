e17484f88d9195acf21e361f55444e2381fd4485
Rasukarusan <gta_youtube@yahoo.co.jp>
2021-03-27 11:45:24 +0900

    add middleware

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,8 +1,22 @@
-import { Module } from '@nestjs/common'
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common'
 import { TerminalModule } from './terminal/terminal.module'
 import { TermIoModule } from './term-io/term-io.module'
+import { logger } from './logger.middleware'
+import { TerminalController } from './terminal/terminal.controller'
 
 @Module({
   imports: [TerminalModule, TermIoModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(logger)
+      .exclude({ path: 'terminals/dispose', method: RequestMethod.POST })
+      .forRoutes(TerminalController)
+  }
+}
--- packages/server/src/terminal/terminal.module.ts
+++ packages/server/src/terminal/terminal.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { TerminalService } from './terminal.service';
-import { TerminalController } from './terminal.controller';
+import { Module } from '@nestjs/common'
+import { TerminalService } from './terminal.service'
+import { TerminalController } from './terminal.controller'
 
 @Module({
   controllers: [TerminalController],
-  providers: [TerminalService]
+  providers: [TerminalService],
 })
 export class TerminalModule {}
